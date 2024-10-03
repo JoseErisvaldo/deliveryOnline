0186b0141256649eb9e094994e4ba37aeb6cf3bd
JoseErisvaldo <joseerisvaldopg@gmail.com>
2024-10-03 14:09:16 -0300

    style: adjusting styles in header

--- components/layout/header/header.js
+++ components/layout/header/header.js
@@ -3,23 +3,16 @@ import { ShoppingBag, User } from 'lucide-react';
 
 export default function Header() {
   return (
-    <div className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-14 items-center">
-        <div className="mr-4 hidden md:flex">Logo</div>
-        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
-          <nav className="flex items-center space-x-6">
-            <Button>Restaurantes</Button>
-            <Button>Mercados</Button>
-            <Button>Bebidas</Button>
-          </nav>
-          <div className="flex items-center space-x-4">
+    <div className="sticky top-0 z-50 w-full border-b bg-light">
+      <div className="flex justify-between">
+        <div className="m-4 font-bold">Delivery Online</div>
+        <div className="m-4 flex items-center">
+          <div className="flex items-center gap-3">
             <Button size="icon">
               <ShoppingBag className="h-5 w-5" />
-              <span className="sr-only">Carrinho</span>
             </Button>
             <Button size="icon">
               <User className="h-5 w-5" />
-              <span className="sr-only">Conta</span>
             </Button>
           </div>
         </div>
