5e739f9d003db171c4b64110d1e9ce52de8b8735
JoseErisvaldo <joseerisvaldopg@gmail.com>
2024-10-03 15:32:20 -0300

    style: adjusting styles in cover

--- components/layout/cover/cover.js
+++ components/layout/cover/cover.js
@@ -3,15 +3,15 @@ import { Search } from 'lucide-react';
 
 export default function Cover() {
   return (
-    <div className="w-full py-12 md:py-24 lg:py-32 bg-primary">
+    <div className="w-full py-12 md:py-24 lg:py-32 bg-[#2f70d1] rounded-3xl">
       <div className="px-4 md:px-6">
         <div className="flex flex-col items-center space-y-4 text-center">
           <div className="space-y-2">
             <h1 className="text-3xl font-bold tracking-tighter text-white sm:text-4xl md:text-5xl lg:text-6xl">
-              Peça seu delivery
+              Realize seu pedido agora !! 
             </h1>
             <p className="mx-auto max-w-[700px] text-white md:text-xl">
-              Os melhores restaurantes com os melhores preços, entregues na sua
+              Os melhores estabelecimento com os melhores preços, entregues na sua
               porta.
             </p>
           </div>
