a82abba2bd6bb11bb87afa8c078dbe44af35183f
JoseErisvaldo <joseerisvaldopg@gmail.com>
2024-10-03 13:36:29 -0300

    Feat: name site

--- components/layout/footer/footer.js
+++ components/layout/footer/footer.js
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="w-full py-6 bg-gray-800 text-white">
       <div className="container px-4 md:px-6 flex justify-between items-center">
-        <p>© 2024 iFood Clone. Todos os direitos reservados.</p>
+        <p>© 2024 DeliveryOnline. Todos os direitos reservados.</p>
         <nav className="space-x-4">
           <a href="#" className="hover:underline">
             Sobre
