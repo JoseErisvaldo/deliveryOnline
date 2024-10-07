a92c0d5b20d512087b1766c0f1c2c79268b8c9d2
JoseErisvaldo <joseerisvaldopg@gmail.com>
2024-10-07 10:18:44 -0300

    Feat: page & components Establishments

--- components/layout/establishment/establishment.js
+++ components/layout/establishment/establishment.js
@@ -1,26 +1,74 @@
+import { fetchSupabase } from '@/pages/api/base/GET/fetchSupabase';
+import { Heart } from 'lucide-react';
 import Link from 'next/link';
+import { useState, useEffect } from 'react'; // Adicione esta linha
 
 export default function Establishment() {
+  const [establishments, setEstablishments] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const getEstablishments = async () => {
+      try {
+        const data = await fetchSupabase(
+          '/rest/v1/view_establishment',
+          'select=*'
+        );
+        console.log(data);
+        setEstablishments(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getEstablishments();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
     <div className="w-full py-12 md:py-24 lg:py-32 bg-gray-100">
       <div className="px-4 md:px-6">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          {[1, 2, 3, 4, 5, 6, 7, 8].map((i) => (
+          {establishments.map((item) => (
             <div
-              key={i}
+              key={item.establishment_id}
               className="rounded-lg overflow-hidden shadow-lg bg-white"
             >
-              <Link href={`/restaurants/${i}`}>
-                <div className="p-4">
-                  <h3 className="font-bold text-lg mb-2">Restaurante {i}</h3>
-                  <p className="text-gray-600 text-sm mb-2">
-                    Especialidade • $$
-                  </p>
-                  <p className="text-gray-600 text-sm">
-                    30-45 min • Entrega: R$ 5,99
-                  </p>
+              <div className="relative bg-white shadow-md rounded-lg cursor-pointer overflow-hidden">
+                <img
+                  src={item.photo_establishment}
+                  alt={`${item.name_establishment} Photo`}
+                  className="absolute inset-0 w-full h-full object-cover opacity-30"
+                />
+
+                <div className="relative z-10 p-4">
+                  <h3 className="font-bold text-lg flex justify-between items-center">
+                    <div>{item.name_establishment}</div>
+                    <div className="cursor-pointer hover:scale-110">
+                      {' '}
+                      <Heart />
+                    </div>
+                  </h3>
+                  <Link href={`/establishments/${item.establishment_id}`}>
+                    <p className="text-black text-sm">
+                      <strong>Especialidade •</strong> {item.specialty}
+                    </p>
+                    <p className="text-black text-sm">
+                      <strong>Entrega entre •</strong> {item.minimumTime} -{' '}
+                      {item.maximumTime} minutos
+                    </p>
+                    <p className="text-black text-sm">
+                      <strong>Endreço•</strong> {item.address_establishment} -{' '}
+                      {item.maximumTime} minutos
+                    </p>
+                  </Link>
                 </div>
-              </Link>
+              </div>
             </div>
           ))}
         </div>
--- components/layout/establishmentProfile/establishmentProfile.js
+++ components/layout/establishmentProfile/establishmentProfile.js
@@ -1,88 +1,95 @@
+// Exemplo de componente atualizado
 import Button from '@/components/ui/button/button';
 import { Clock, Info, MapPin, ShoppingBag, Star } from 'lucide-react';
 
-export default function EstablishmentProfile() {
-  const restaurantData = {
-    name: 'Pizzaria Bella Napoli',
-    photo: 'https://www.designi.com.br/images/preview/12870455.jpg',
-    rating: 4.7,
-    category: 'Italiana',
-    deliveryTime: '30-45',
-    deliveryFee: '5.99',
-    minOrder: '20.00',
-    address: 'Rua das Pizzas, 123 - Centro',
-    description:
-      'Autêntica pizza napolitana feita em forno a lenha. Ingredientes frescos e de qualidade.',
-  };
+export default function EstablishmentProfile({ establishment }) {
+  // Verifica se o estabelecimento está definido e se é um array
+  if (!establishment || establishment.length === 0)
+    return <div>Loading...</div>; // Se não houver estabelecimento, mostra um carregando
+
+  const data = establishment[0]; // Acessa o primeiro elemento do array
 
   return (
     <div>
-      <div className="">
-        <img
-          className="w-screen h-52 object-cover rounded-2xl"
-          src={restaurantData.photo}
-        />
-      </div>
-      <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-6">
-        <div>
-          <h2 className="text-2xl font-bold mb-2">{restaurantData.name}</h2>
-          <div className="flex items-center space-x-2 text-sm text-gray-600">
-            <span>{restaurantData.category}</span>
-            <span>•</span>
-            <div className="flex items-center">
-              <Star className="h-4 w-4 text-yellow-400 mr-1" />
-              <span>{restaurantData.rating}</span>
-            </div>
+      {data ? (
+        <>
+          <div>
+            <img
+              className="w-screen h-52 object-cover rounded-2xl"
+              src={data.photo_establishment || '/default-image.jpg'} // Use uma imagem padrão se não houver
+              alt={data.name_establishment}
+            />
           </div>
-        </div>
-        <Button variant="outline" className="mt-4 md:mt-0">
-          <Info className="h-4 w-4 mr-2" />
-          Mais informações
-        </Button>
-      </div>
+          <div className="flex flex-col md:flex-row justify-between items-start md:items-center mb-6">
+            <div>
+              <h2 className="text-2xl font-bold mb-2">
+                {data.name_establishment || 'Nome não disponível'}
+              </h2>
+              <div className="flex items-center space-x-2 text-sm text-gray-600">
+                <span>
+                  {data.specialty_establishment || 'Categoria não disponível'}
+                </span>
+                <span>•</span>
+                <div className="flex items-center">
+                  <Star className="h-4 w-4 text-yellow-400 mr-1" />
+                  <span>{data.rating || 'Rating não disponível'}</span>
+                </div>
+              </div>
+            </div>
+            <Button variant="outline" className="mt-4 md:mt-0">
+              <Info className="h-4 w-4 mr-2" />
+              Mais informações
+            </Button>
+          </div>
 
-      <div className="mb-6">
-        <div className="p-4">
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-            <div className="flex items-center">
-              <Clock className="h-5 w-5 mr-2 text-gray-600" />
-              <div>
-                <p className="text-sm font-medium">Tempo de entrega</p>
-                <p className="text-sm text-gray-600">
-                  {restaurantData.deliveryTime} min
-                </p>
-              </div>
-            </div>
-            <div className="flex items-center">
-              <ShoppingBag className="h-5 w-5 mr-2 text-gray-600" />
-              <div>
-                <p className="text-sm font-medium">Pedido mínimo</p>
-                <p className="text-sm text-gray-600">
-                  R$ {restaurantData.minOrder}
-                </p>
-              </div>
-            </div>
-            <div className="flex items-center">
-              <MapPin className="h-5 w-5 mr-2 text-gray-600" />
-              <div>
-                <p className="text-sm font-medium">Endereço</p>
-                <p className="text-sm text-gray-600">
-                  {restaurantData.address}
-                </p>
-              </div>
-            </div>
-            <div className="flex items-center">
-              <ShoppingBag className="h-5 w-5 mr-2 text-gray-600" />
-              <div>
-                <p className="text-sm font-medium">Taxa de entrega</p>
-                <p className="text-sm text-gray-600">
-                  R$ {restaurantData.deliveryFee}
-                </p>
+          <div className="mb-6">
+            <div className="p-4">
+              <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+                <div className="flex items-center">
+                  <Clock className="h-5 w-5 mr-2 text-gray-600" />
+                  <div>
+                    <p className="text-sm font-medium">Tempo de entrega</p>
+                    <p className="text-sm text-gray-600">
+                      {data.minimumTime
+                        ? `${data.minimumTime} - ${data.maximumTime} minutos`
+                        : 'Tempo não disponível'}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <ShoppingBag className="h-5 w-5 mr-2 text-gray-600" />
+                  <div>
+                    <p className="text-sm font-medium">Pedido mínimo</p>
+                    <p className="text-sm text-gray-600">
+                      R$ {data.minimumOrder || 'Não disponível'}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <MapPin className="h-5 w-5 mr-2 text-gray-600" />
+                  <div>
+                    <p className="text-sm font-medium">Endereço</p>
+                    <p className="text-sm text-gray-600">
+                      {data.address_establishment || 'Endereço não disponível'}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex items-center">
+                  <ShoppingBag className="h-5 w-5 mr-2 text-gray-600" />
+                  <div>
+                    <p className="text-sm font-medium">Taxa de entrega</p>
+                    <p className="text-sm text-gray-600">
+                      R$ {data.deliveryFee || 'Não disponível'}
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        <div>No establishment found.</div>
+      )}
     </div>
   );
 }
